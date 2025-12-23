be7ad9200fb94fa800ab216705e8e72b84c17e37
hoskim <hosu@outlook.cz>
2025-12-23 21:35:29 +0900

    Implement Fixed class with constructors, assignment operator, and destructor

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,45 @@
+#include "Fixed.hpp"
+
+// constructor
+Fixed::Fixed() : _fixedPointValue(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+// a special constructor
+Fixed::Fixed(const int value) {
+}
+
+// copy constructor
+Fixed::Fixed(const Fixed& other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+// copy assignment operator overload (복사 대입 연산자 오버로딩)
+// operator는 C++ 키워드임
+// : 연산자가 어떻게 작동할지 내가 직접 정의하겠음.
+//   Fixed 객체끼리는 =가 어떻게 동작할지 정의함.
+Fixed& Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+
+	if (this != &other) {
+		this->_fixedPointValue = other._fixedPointValue;
+	}
+	return *this;
+}
+
+// destructor
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+// const: 읽기 전용
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+
+	return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw) {
+	this->_fixedPointValue = raw;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int _fixedPointValue;
+		static const int _fractionalBits = 8;
+	public:
+		Fixed(); // itializes the fixed-point number value to 0. [X]
+		// A constructor that takes a constant integer as a parameter.
+		// It converts it to the corresponding fixed-point value.
+		// The fractional bits value should be initialized to 8.
+		Fixed(const int value);
+		// copy constructor [X]
+		Fixed(const Fixed& other);
+		// copy assignment operator overload
+		// 복사 대입 연산자 오버로딩
+		Fixed& operator=(const Fixed& other);
+		// destructor
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+#endif
